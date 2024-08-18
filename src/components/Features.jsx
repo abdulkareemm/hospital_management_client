a487e8557374916dcffb3b44ee547d560c678939
abdulkareemm <eng.kreem.96@gmail.com>
2024-08-18 10:56:43 +0300

    add feature component

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const Features = () => {
+const Features = ({ name, image }) => {
   return (
-    <div>Features</div>
-  )
-}
+    <div className="relative w-[550px]  overflow-hidden group shadow-[4_20px_14px_rgba(141,141,141,0.25)]">
+      <img
+        src={image}
+        alt={`${name}/img`}
+        className="w-[270px] rounded-2xl group-hover:opacity-100 opacity-60 cursor-pointer transition-all duration-200"
+      />
+      <div
+        className="absolute top-[76%] left-[12%] bg-[#10defd] w-[130px] h-10 flex items-center justify-center rounded-2xl text-white 
+      uppercase group-hover:opacity-100 opacity-60"
+      >
+        <h1>{name}</h1>
+      </div>
+    </div>
+  );
+};
 
 export default Features
